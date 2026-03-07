d89cce5be4bd7e770b7e20af13d2a6fe8f9fa55e
aqua <aoi.kiyohara0306@gmail.com>
2026-03-07 23:53:51 +0900

    feature: add psd character component

---
+++ src/lib/character/psd-character-component.ts
@@ -0,0 +1,31 @@
+import type { Variable } from "../animation"
+import { defineDSL } from "./defineDSL"
+
+export const MotionSequence = defineDSL<{
+  children: React.ReactNode
+}>("MotionSequence")
+
+export const DeclareVariable = defineDSL<{
+  variableName: string
+  initValue: any
+  children: React.ReactNode
+}>("DeclareVariable")
+
+export const Block = defineDSL<{
+  children: React.ReactNode
+}>("Block")
+
+export const Animation = defineDSL<{
+  f: (ctx: any, variable: Record<string, Variable<any>>) => Promise<void>
+  children?: React.ReactNode
+}>("Animation")
+
+export const Voice = defineDSL<{
+  src: string
+  volume?: number
+  children?: React.ReactNode
+}>("Voice")
+
+export const Motion = defineDSL<{
+  motion: (variables: Record<string, Variable<any>>, frames: number[]) => Record<string, any>
+}>("Motion")
